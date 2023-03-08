93d0fc40392dd93f2df4146c2e3c2cfdc6bf5c83
huzaifak08 <hk7928042@gmail.com>
2023-03-08 22:04:50 +0500

    Background Notification Done Allhamdullillah

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:test_app/Splash/splash_page.dart';
-import 'package:test_app/home.dart';
-import 'package:test_app/Auth%20Screens/login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  // Initialize background notification:
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(const MyApp());
+}
+
+// Must initialize this for Background Notification:
+@pragma('vm:entry-point') // Must Add this:
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+
+  debugPrint(message.notification!.title.toString());
 }
 
 class MyApp extends StatelessWidget {
