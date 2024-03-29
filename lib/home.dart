be91c83c0e103f14d29e65f34a5df811b522eb57
huzaifak08 <hk7928042@gmail.com>
2024-03-29 12:22:13 +0500

    Email Send Allhamdullillah

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final toController = TextEditingController();
+  final messageController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sending Email"),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: toController,
+            decoration: const InputDecoration(labelText: 'To'),
+          ),
+          TextField(
+            controller: messageController,
+            decoration: const InputDecoration(labelText: 'Body'),
+            maxLines: 4,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              sendMail(
+                  recipientEmail: toController.text,
+                  mailMessage: messageController.text);
+            },
+            child: const Text('Send Email'),
+          )
+        ],
+      ),
+    );
+  }
+
+  void sendMail(
+      {required String recipientEmail, required String mailMessage}) async {
+    String username = 'hk7928042@gmail.com';
+
+    String password = 'htvy kgpt gdkx kojf';
+
+    final smtpServer = gmail(username, password);
+
+    final message = Message()
+      ..from = Address(username, 'Mail Service')
+      ..recipients.add(recipientEmail)
+      ..subject = 'Mail'
+      ..text = 'Message $mailMessage';
+
+    try {
+      await send(message, smtpServer);
+      debugPrint("Email Send Successfully");
+    } catch (err) {
+      debugPrint(err.toString());
+    }
   }
 }
