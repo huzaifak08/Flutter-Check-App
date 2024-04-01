b909b3e7be53b7c679565b4fde18a21f73b16f2d
huzaifak08 <hk7928042@gmail.com>
2024-04-01 15:36:31 +0500

    Responsive Extension Allhamdullillah

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
 
+extension SizeExtension on BuildContext {
+  SizedBox height(double factor) {
+    return SizedBox(height: MediaQuery.of(this).size.height * factor);
+  }
+
+  SizedBox width(double factor) {
+    return SizedBox(height: MediaQuery.of(this).size.width * factor);
+  }
+}
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.red,
+          ),
+          context.height(0.2),
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.yellow,
+          ),
+        ],
+      ),
+    );
   }
 }
